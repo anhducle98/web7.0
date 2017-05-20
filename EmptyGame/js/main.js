8c7262c80243418146be22d7c7d1e3f6e20369c8
lad <anhducle98@gmail.com>
2017-05-21 01:36:22 +0700

    move background

--- EmptyGame/js/main.js
+++ EmptyGame/js/main.js
@@ -3,12 +3,12 @@ Nakama.configs = {};
 
 window.onload = function(){
   Nakama.game = new Phaser.Game(640,960,Phaser.AUTO,'',
-    {
-      preload: preload,
-      create: create,
-      update: update,
-      render: render
-    }, false, false
+  {
+    preload: preload,
+    create: create,
+    update: update,
+    render: render
+  }, false, false
   );
 }
 
@@ -32,47 +32,53 @@ var create = function(){
   Nakama.game.physics.startSystem(Phaser.Physics.ARCADE);
   Nakama.keyboard = Nakama.game.input.keyboard;
 
-  var background = Nakama.game.add.sprite(0, 0, "background");
+  Nakama.upper_background = Nakama.game.add.sprite(0, 0, "background");
+  Nakama.lower_background = Nakama.game.add.sprite(0, 0, "background");
+  upper_crop = new Phaser.Rectangle(0, 0, Nakama.game.width, Nakama.game.height);
+  lower_crop = new Phaser.Rectangle(0, 0, Nakama.game.width, Nakama.game.height);
+  Nakama.upper_background.crop(upper_crop);
+  Nakama.lower_background.crop(lower_crop);
   Nakama.player = Nakama.game.add.sprite(300, 400, "assets", "Spaceship1-Player.png");
+  Nakama.player.velocity = 10;
 }
 
-velocity = 1;
-gain = 0.5;
-last_dir = -1;
+var get_high = function() {
+  if (Nakama.keyboard.isDown(Phaser.Keyboard.UP)) {
+    Nakama.player.y -= Nakama.player.velocity;
+  }
+  if (Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)) {
+    Nakama.player.y += Nakama.player.velocity;
+  }
+  if (Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)) {
+    Nakama.player.x -= Nakama.player.velocity;
+  }
+  if (Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
+    Nakama.player.x += Nakama.player.velocity;
+  }
+  Nakama.player.x = Math.max(Nakama.player.x, 0);
+  Nakama.player.y = Math.max(Nakama.player.y, 0);
+  Nakama.player.x = Math.min(Nakama.game.width - Nakama.player.width, Nakama.player.x);
+  Nakama.player.y = Math.min(Nakama.game.height - Nakama.player.height, Nakama.player.y);
+}
+
+var shift = 0;
+var background_dance = function() {
+  shift += 3;
+  if (shift >= Nakama.game.height) {
+    shift -= Nakama.game.height
+  }
+  upper_crop.y = Nakama.game.height - shift;
+  upper_crop.height = shift;
+  lower_crop.height = Nakama.game.height - shift;
+  Nakama.lower_background.y = shift;
+  Nakama.upper_background.updateCrop();
+  Nakama.lower_background.updateCrop();  
+}
 
 // update game state each frame
 var update = function(){
-    //Nakama.player.x += (Math.random() - Math.random()) * 10;
-    //Nakama.player.y += (Math.random() - Math.random()) * 10;
-    new_dir = -1;
-
-    if (Nakama.keyboard.isDown(Phaser.Keyboard.UP)) {
-        new_dir = 0;
-        if (last_dir === new_dir) velocity += gain;
-        Nakama.player.y -= velocity;
-    }
-    if (Nakama.keyboard.isDown(Phaser.Keyboard.DOWN)) {
-        new_dir = 1;
-        if (last_dir === new_dir) velocity += gain;
-        Nakama.player.y += velocity;
-    }
-    if (Nakama.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-        new_dir = 2;
-        if (last_dir === new_dir) velocity += gain;
-        Nakama.player.x -= velocity;
-    }
-    if (Nakama.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-        new_dir = 3;
-        if (last_dir === new_dir) velocity += gain;
-        Nakama.player.x += velocity;
-    }
-    if (last_dir != new_dir) velocity = 1;
-    last_dir = new_dir;
-
-    Nakama.player.x = Math.max(Nakama.player.x, 0);
-    Nakama.player.y = Math.max(Nakama.player.y, 0);
-    Nakama.player.x = Math.min(Nakama.game.width - Nakama.player.width, Nakama.player.x);
-    Nakama.player.y = Math.min(Nakama.game.height - Nakama.player.height, Nakama.player.y);
+  get_high();
+  background_dance();
 }
 
 // before camera render (mostly for debug)
