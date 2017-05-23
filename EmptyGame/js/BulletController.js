20cd18706e479328323f233596b00eb7350456e4
lad <anhducle98@gmail.com>
2017-05-24 00:21:20 +0700

    ahihi

---
+++ EmptyGame/js/BulletController.js
@@ -0,0 +1,14 @@
+class BulletController {
+    constructor(x, y, fileName) {
+        this.sprite = Nakama.game.add.sprite(x, y, "assets", fileName);
+        this.display = true;
+    }
+
+    update() {
+        this.sprite.y -= 17;
+        if (this.sprite.y < 0) {
+            this.display = false;
+            this.sprite.destroy();
+        }
+    }
+}
